251ee34102e6c32ea9370b89df6ee5db3baf3c31
guillaume prof <guillaume.saintetienne@iter.org>
2025-12-19 15:33:28 +0100

    todo: remplacer Database par repository

---
+++ test-types/chapter-a-prime/contraint-on-types.test.ts
@@ -0,0 +1,27 @@
+import {
+    AllowedProduct,
+    DangerousProduct,
+    DigitalProduct,
+    EAN13,
+    ForbiddenProduct,
+    PhysicalProduct
+} from "./domain/product";
+
+
+describe('Contraint on types', () => {
+    it('should ', () => {
+
+
+        let authorizedProducts: Array<AllowedProduct | ForbiddenProduct> = [];
+        let forbiddenProducts: ForbiddenProduct[] = [];
+
+        authorizedProducts.push(new PhysicalProduct('1', 'Product 1', 10));
+        authorizedProducts.push(new DigitalProduct('2', 'Product 2', 20));
+       authorizedProducts.push(new DangerousProduct('3', 'Product 3', 30));
+
+        let catalogue : Map<EAN13, AllowedProduct > = new Map();
+
+        catalogue.set(new EAN13('1234567890123'), new PhysicalProduct('1', 'Product 1', 10));
+        catalogue.set(new EAN13('1234567890124'), new DangerousProduct('2', 'Product 2', 20));
+    });
+});
--- test-types/chapter-a-prime/domain/product.ts
+++ test-types/chapter-a-prime/domain/product.ts
@@ -5,9 +5,19 @@ export interface Product {
   getPrice(): number;
 }
 
+export interface ForbiddenProduct extends Product {
+  readonly  __brand: 'forbidden';
+}
+
+export interface AllowedProduct extends Product {
+  readonly  __brand: 'allowed';
+}
+
 // Implementations
-export class PhysicalProduct implements Product {
+export class PhysicalProduct implements AllowedProduct {
   constructor(readonly id: string, readonly name: string, readonly price: number) {}
+
+  __brand : "allowed" = "allowed";
 
    private TRANSPORT_FEES_RATE = 0.1;
 
@@ -16,10 +26,33 @@ export class PhysicalProduct implements Product {
   }
 }
 
-export class DigitalProduct implements Product {
+export class DigitalProduct implements AllowedProduct {
   constructor(readonly id: string, readonly name: string, readonly price: number) {}
+
+  __brand: "allowed" = "allowed";
 
   getPrice(): number {
     return this.price;
   }
 }
+
+export class DangerousProduct implements ForbiddenProduct {
+  constructor(readonly id: string, readonly name: string, readonly price: number) {}
+
+  __brand: "forbidden" = "forbidden";
+
+  getPrice(): number {
+    return this.price;
+  }
+}
+
+
+export class EAN13 {
+    constructor(readonly code: string) {
+      if (!/^[0-9]{13}$/.test(code)) {
+        throw new Error('Invalid EAN13 code');
+      }
+    }
+}
+
+
